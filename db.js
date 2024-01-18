d1298cce4dbf2e0f15beaa64888a19d692a46490
Elana Felty <elanafelty@gmail.com>
2024-01-18 12:57:24 -0600

    40.3 Object-oriented Node-pg

---
+++ db.js
@@ -0,0 +1,21 @@
+/** Database for lunchly */
+
+const pg = require("pg");
+
+let DB_URI;
+
+if(process.env.NODE_ENV === 'test'){
+    DB_URI = 'lunchly_test'
+} else {
+    DB_URI = 'lunchly'
+}
+
+const db = new pg.Client({
+    host: '/var/run/postgresql',
+    database: DB_URI
+});
+
+db.connect();
+
+module.exports = db;
+ 
